e07c83f28d4ab6b6aa14c886ac02d95c05190320
JitanjaliPatel19 <jitanjalipatel19@gmail.com>
2026-03-27 20:51:51 +0530

    Delete prisma directory

--- prisma/migrations/20260326185009_init_schema/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[userId,moduleName]` on the table `role_permissions` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "role_permissions_userId_moduleName_key" ON "role_permissions"("userId", "moduleName");
